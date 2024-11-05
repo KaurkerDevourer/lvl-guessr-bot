d89ae64ba9bbf081b4fa9205b31c637a4e41c9d5
Stepan Styopkin <smstyopkin@edu.hse.ru>
2024-11-05 11:56:54 +0000

    no main

--- main.py
+++ main.py
@@ -6,12 +6,6 @@ with open("token.txt") as file:
     token = file.readline().strip('\n')
 bot = telebot.TeleBot(token)
 
-def main():
-    print("SUCCESS START")
-
-if __name__ == '__main__':
-    main()
-
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
 	bot.reply_to(message, "Howdy, how are you doing?")
