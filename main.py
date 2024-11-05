9276933fb8d5648c01f899c3607de08d35349911
Stepan Styopkin <smstyopkin@edu.hse.ru>
2024-11-05 11:53:57 +0000

    basic

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
+import telebot
 
 
-
+token = "UNKNOWN_TOKEN"
+with open("token.txt") as file:
+    token = file.readline().strip('\n')
+bot = telebot.TeleBot(token)
 
 def main():
-    token = "UNKNOWN_TOKEN"
-    with open("token.txt") as file:
-        for line in file:
-            token = line
-    print("SUCCESS")
+    print("SUCCESS START")
 
 if __name__ == '__main__':
     main()
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, "Howdy, how are you doing?")
+
+bot.infinity_polling()
