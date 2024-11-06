6a9e066f578ae864f3e865da896486d05a6a027d
Niyaz <nmingaripov@gmail.com>
2024-11-06 23:02:48 +0300

    feat UserStatisticsStorage: add specific Get methods

--- db/user_statistics.py
+++ db/user_statistics.py
@@ -16,24 +16,51 @@ class UserStatisticsStorage:
             raw_data = json.loads(file.read())
 
         for (user_id, stat) in raw_data.items():
-            self.data[user_id] = defaultdict(lambda: (0, 0), stat)
-            
+            self.data[user_id] = defaultdict(lambda: (0, 0), tuple(stat.items()))
+
     def Serialize(self):
         s = json.dumps(self.data)
         with open(self.file_name, "w") as file:
             file.write(s)
 
-    def AddWin(self, user_id: str, use_case: Gamemode):
-        """Example of a call: stat.add_win("user_id", UseCase.GUESS_THE_LVL)"""
-        (win, total) = self.data[user_id][use_case.name]
-        self.data[user_id][use_case.name] = (win + 1, total + 1)
+    def AddWin(self, user_id: str, gamemode: Gamemode):
+        (win, total) = self.data[user_id][gamemode.name]
+        self.data[user_id][gamemode.name] = (win + 1, total + 1)
         self.Serialize()
 
-    def AddFail(self, user_id: str, use_case: Gamemode):
-        (win, total) = self.data[user_id][use_case.name]
-        self.data[user_id][use_case.name] = (win, total + 1)
+    def AddFail(self, user_id: str, gamemode: Gamemode):
+        (win, total) = self.data[user_id][gamemode.name]
+        self.data[user_id][gamemode.name] = (win, total + 1)
         self.Serialize()
 
-    def Get(self, user_id: str, use_case: Gamemode):
+    def Get(self, user_id: str, gamemode: Gamemode):
         """Return (win, total)"""
-        return self.data[user_id][use_case.name]
+        return self.data[user_id][gamemode.name]
+
+    def GetRank(self, user_id: str, gamemode: Gamemode):
+        """Return rank"""
+        # TODO: nth_element
+        sorted_data = sorted(self.data.items(), key=lambda x: x[1][gamemode.name][0], reverse=True)
+        for rank, (user_id, _) in enumerate(sorted_data, 1):
+            if user_id == user_id:
+                return rank
+        return None
+
+    def GetSorted(self, gamemode: Gamemode, limit: int = 20):
+        """Return list of tuples (user_id, (win, total, rank)) sorted by win"""
+        if limit <= 0:
+            print(f"ERROR: Limit (which is {limit}) must be positive")
+            return None
+
+        if limit > len(self.data):
+            limit = len(self.data)
+
+        win_idx = 0
+        sorted_data = sorted(self.data.items(), key=lambda x: x[1][gamemode.name][win_idx], reverse=True)[:limit]
+
+        result = []
+        for rank, (user_id, data) in enumerate(sorted_data, 1):
+            win, total = data[gamemode.name]
+            result.append((user_id, (win, total, rank)))
+        return result if len(result) > 0 else None
+
