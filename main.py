6465889e4bcdca59c75a9289016f6c4a06124b09
Niyaz <nmingaripov@gmail.com>
2024-11-06 11:34:37 +0300

    States

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
 import telebot
-from telebot import types
+from telebot import custom_filters, types
+from telebot.states import State, StatesGroup
+from telebot.states.sync.context import StateContext
+from telebot.storage import StateMemoryStorage
 
 import json
+
 
 token = "UNKNOWN_TOKEN"
 with open("token.txt") as file:
     token = file.readline().strip('\n')
-bot = telebot.TeleBot(token)
+state_storage = StateMemoryStorage()
+bot = telebot.TeleBot(token, state_storage=state_storage, use_class_middlewares=True)
 
 with open("questions.json") as file:
     questions = json.load(file)
 
+class GameStates(StatesGroup):
+    gamemode_selecting = State()
+
+class GTLStates(StatesGroup):
+    guessing = State()
+    answering = State()
+    choosing = State()
+
+class HvsAIStates(StatesGroup):
+    choosing = State()
+    guessing = State()
+    answering = State()
 
 users_states = {}
 
@@ -34,23 +51,44 @@ def get_question_by_id(question_id) -> json:
         return None
     return questions[question_id]
 
-@bot.message_handler(commands=['guess'])
-def start_the_game(message):
-    bot.send_message(message.from_user.id, 'Поехали! 🚀')
 
+@bot.message_handler(state=GTLStates.choosing)
+def choose_gtl(message, state: StateContext):
+    if message.text == "Давай дальше!":
+        guess_GTL(message, state)
+    elif message.text == "Нет, хватит.":
+        state.set(GameStates.gamemode_selecting)
+        select_gamemode_message(message, state)
+
+def choose_next_action(message, state: StateContext):
+    state.set(GTLStates.choosing)
+
+    btn1 = types.KeyboardButton('Давай дальше!')
+    btn2 = types.KeyboardButton('Нет, хватит.')
+
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    markup.add(btn1, btn2)
+    bot.send_message(message.from_user.id, "Ещё одну? 😉", reply_markup=markup)
+
+@bot.message_handler(state=GTLStates.answering)
+def answer_gtl(message, state: StateContext):
     user_id = message.from_user.id
-    question_id = get_next_question_id(user_id)
-    question = get_question_by_id(question_id)
-    if question == None:
-        bot.send_message(user_id, "Вопросы закончились 😢")
+    user_state = users_states.get(user_id)
+
+    if user_state == None:
+        bot.send_message(user_id, "Что-то пошло не так -_-")
         return
+    
+    if message.text == user_state["correct_answer"]:
+        bot.send_message(user_id, "Верно! 🎉\n" + user_state["question"]["link"] + "\n" + user_state["question"]["author"])
+        users_states[user_id] = None
 
-    users_states[user_id] = {
-        "question": question,
-        "correct_answer": question["level"]
-    }
+        choose_next_action(message, state)
+    else:
+        bot.send_message(user_id, "Неверно ¯\_(ツ)_/¯. Попробуй ещё раз.")
 
-    bot.send_message(message.from_user.id, question["code"])
+def guess_the_level_buttons(message, state: StateContext, question):
+    state.set(GTLStates.answering)
 
     levels = ["Junior", "Middle", "Senior", "Lead"]
     years = ["0-1 years", "1-3 years", "3-6 years", "6+ years"]
@@ -63,24 +101,59 @@ def start_the_game(message):
     markup.add(*buttons)
     bot.send_message(message.from_user.id, "Quess the level!", reply_markup=markup)
 
-@bot.message_handler(content_types=['text'])
-def on_message_create(message):
-
+def guess_GTL(message, state):
     user_id = message.from_user.id
-    state = users_states.get(user_id)
+    question_id = get_next_question_id(user_id)
+    question = get_question_by_id(question_id)
+    if question == None:
+        bot.send_message(user_id, "Вопросы закончились 😢")
+        
+        select_gamemode_message(message, state)
+        return
 
-    if state:
-        if message.text == state["correct_answer"]:
-            bot.send_message(user_id, "Верно! 🎉\n" + state["question"]["link"] + "\n" + state["question"]["author"])
-            users_states[user_id] = None
-        else:
-            bot.send_message(user_id, "Неверно ¯\_(ツ)_/¯. Попробуй ещё раз.")
-    else:
-        bot.send_message(user_id, "Начни игру, используя команду /guess.")
+    users_states[user_id] = {
+        "question": question,
+        "correct_answer": question["level"]
+    }
+
+    bot.send_message(message.from_user.id, question["code"])
+
+    guess_the_level_buttons(message, state, question)
+
+def guess_HvsAI(message, state: StateContext):
+    bot.send_message(message.from_user.id, "Not implemented yet")
+    
+    select_gamemode_message(message, state)
+
+@bot.message_handler(state=GameStates.gamemode_selecting)
+def gamemode_selecting(message, state: StateContext):
+    if message.text == "Guess the Level":
+        bot.send_message(message.from_user.id, 'Поехали! 🚀')
+        state.set(GTLStates.guessing)
+        guess_GTL(message, state)
+    elif message.text == "Human vs AI":
+        bot.send_message(message.from_user.id, 'Поехали! 🚀')
+        state.set(HvsAIStates.guessing)
+        guess_HvsAI(message, state)
+
+def select_gamemode_message(message, state: StateContext):
+    state.set(GameStates.gamemode_selecting)
+
+    gamemodes = ["Guess the Level", "Human vs AI"]
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
+    markup.add(*[types.KeyboardButton(mode) for mode in gamemodes])
+    bot.send_message(message.from_user.id, "Выберите режим игры:", reply_markup=markup)
 
 @bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-    bot.send_message(message.from_user.id, "Добро пожаловать в игру Guess the Level! 🚀\n\n")
-    bot.send_message(message.from_user.id, "Чтобы начать, наберите /guess")
+def send_welcome(message, state: StateContext):
+    bot.send_message(message.from_user.id, "Добро пожаловать в игру Guess the Something! 🚀\n\n")
+
+    select_gamemode_message(message, state)
+
+bot.add_custom_filter(custom_filters.StateFilter(bot))
+
+from telebot.states.sync.middleware import StateMiddleware
+
+bot.setup_middleware(StateMiddleware(bot))
 
 bot.infinity_polling()
